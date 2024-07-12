33200db57a28e36f307dc7ec37c13c82c8c2d175
Yuriy Lutsenko <lutsenko.yuriy93@gmail.com>
2024-07-12 12:12:11 +0200

    Initial commit

---
+++ src/main/kotlin/com/iurii/thymeleafdemo/ThymeleafDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.iurii.thymeleafdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ThymeleafDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<ThymeleafDemoApplication>(*args)
+}
