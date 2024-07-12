b12ab1494307e726e2c8f4fc21861cdf5373b274
Yuriy Lutsenko <lutsenko.yuriy93@gmail.com>
2024-07-12 23:24:05 +0200

    Add shout method

--- src/main/kotlin/com/iurii/thymeleafdemo/controller/HelloWorldController.kt
+++ src/main/kotlin/com/iurii/thymeleafdemo/controller/HelloWorldController.kt
@@ -1,6 +1,8 @@
 package com.iurii.thymeleafdemo.controller
 
+import jakarta.servlet.http.HttpServletRequest
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
@@ -13,7 +15,16 @@ class HelloWorldController {
     }
 
     @RequestMapping("/submit")
-    fun submitForm(): String {
+    fun submitForm(request: HttpServletRequest, model: Model): String {
+        val message = request.getParameter("studentName")
+        model.addAttribute("message", message)
+        return "helloworld"
+    }
+
+    @RequestMapping("/shout")
+    fun shoutForm(request: HttpServletRequest, model: Model): String {
+        val message = request.getParameter("studentName").uppercase()
+        model.addAttribute("message", "Yo! $message")
         return "helloworld"
     }
 }
